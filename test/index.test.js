c3bfc43047565c481c8b8db531d01325181bf26a
IDONTSUDO <idontsudo@gmail.com>
2020-08-06 00:36:50 +0300

    adding test

---
+++ test/index.test.js
@@ -0,0 +1,12 @@
+const httpFunction = require('../HttpTrigger1Test/index');
+const context = require('./lib/defaultContext')
+const mock = require('./mocks/index.json')
+test('Http trigger should return known text', async () => {
+
+    const request = mock
+
+    await httpFunction(context, request);
+
+    expect(context.log.mock.calls.length).toBe(1);
+    expect(context.res.body).toEqual('Hello Bill');
+});
